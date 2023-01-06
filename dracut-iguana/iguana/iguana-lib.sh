d4b9f788c461bb93f28fb681c6bfedd8d1772ef4
Ondrej Holecek <oholecek@suse.com>
2023-01-06 15:21:35 +0100

    Shellcheck and other fixes

--- dracut-iguana/iguana/iguana-lib.sh
+++ dracut-iguana/iguana/iguana-lib.sh
@@ -10,7 +10,7 @@ fi
 function iguana_reboot_action() {
   # Always do reboot, only in case of kexec action try kexec first
   # Kexec must be already prepared
-  if [ "kexec" == "$1"]; then
+  if [ "kexec" == "$1" ]; then
     umount -a
     sync
     kexec -e
@@ -20,7 +20,7 @@ function iguana_reboot_action() {
   reboot -f -d -n
 }
 
-function guess_root_mount(newroot) {
+function guess_root_mount() {
   # look for first disk partitition with known root PARTUUIDs
   # see https://uapi-group.org/specifications/specs/discoverable_partitions_specification/
   # Supported currently are:
@@ -28,8 +28,9 @@ function guess_root_mount(newroot) {
   #                 SD_GPT_ROOT_ARM64
   #   amd64/x86_64: 4f68bce3-e8cd-4db1-96e7-fbcaf984b709
   #                 SD_GPT_ROOT_X86_64
-  local supported_uuids = "b921b045-1df0-41c3-af44-4c6f280d3fae SD_GPT_ROOT_ARM64 4f68bce3-e8cd-4db1-96e7-fbcaf984b709 SD_GPT_ROOT_X86_64"
-  local found_root=`lsblk -o PARTUUID,FSTYPE -n -l -J | jq '.blockdevices | map(select(.partuuid != null)) | .[0].partuuid'`
+  supported_uuids="b921b045-1df0-41c3-af44-4c6f280d3fae SD_GPT_ROOT_ARM64 4f68bce3-e8cd-4db1-96e7-fbcaf984b709 SD_GPT_ROOT_X86_64"
+  found_root=$(lsblk -o PARTUUID,FSTYPE -n -l -J | jq '.blockdevices | map(select(.partuuid != null)) | .[0].partuuid')
+  newroot="$1"
 
   if echo "$supported_uuids" | grep -q "$found_root"; then
     # First found partition with UUID is known UUID for root partition.
@@ -39,12 +40,12 @@ function guess_root_mount(newroot) {
   return 1
 }
 
-function is_root_encrypted(device) {
-  local parsed_device="$device"
+function is_root_encrypted() {
+  device="$1"
   if echo "$device" | grep -q "^UUID="; then
-      parsed_device="/dev/disk/by-partuuid/${device#UUID=}"
+      device="/dev/disk/by-partuuid/${device#UUID=}"
   fi
-  local partFS=`lsblk -o FSTYPE -n -l $parsed_device`
-  [ $partFS == "crypto_LUKS" ] && return 0
+  partFS=$(lsblk -o FSTYPE -n -l "$device")
+  [ "$partFS" == "crypto_LUKS" ] && return 0
   return 1
 }
