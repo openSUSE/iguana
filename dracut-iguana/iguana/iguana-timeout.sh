29ae0f0a7bac157c183e998f90ba91c6d55c0f12
Ondrej Holecek <oholecek@suse.com>
2023-06-30 15:01:04 +0200

    Fix scripts to pass shellcheck

--- dracut-iguana/iguana/iguana-timeout.sh
+++ dracut-iguana/iguana/iguana-timeout.sh
@@ -2,13 +2,14 @@
 
 type getarg >/dev/null 2>&1 || . /lib/dracut-lib.sh
 
-if [ -z "$root" ] || [ "$root"x == "iguanabootx" ]; then
+if [ -z "$root" ] || [ "$root"x = "iguanabootx" ]; then
   exit 0
 fi
 
 _name="$(str_replace "${root#block:}" '/' '\x2f')"
 for f in wait-network.sh "devexists-$_name.sh"; do
-  if [ ! -e "$hookdir/initqueue/finished/\$f" ] || ( . "$hookdir/initqueue/finished/\$f" ); then
+  # shellcheck disable=SC2154 disable=SC1090
+  if [ ! -e "$hookdir/initqueue/finished/$f" ] || ( . "$hookdir/initqueue/finished/$f" ); then
     rm -f -- "$hookdir/initqueue/finished/wait-network.sh"
     rm -f -- "$hookdir/initqueue/finished/devexists-$_name.sh"
   fi
